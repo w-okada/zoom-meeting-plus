a0224d57e91fdd6da85b9af3269131768026c7a9
wataru okada <mail2wokada@gmail.com>
2022-12-17 19:30:01 +0900

    clean audio root

--- frontend/src/002_hooks/010_useAudioRoot.ts
+++ frontend/src/002_hooks/010_useAudioRoot.ts
@@ -1,8 +1,7 @@
-import { useMemo, useState } from "react"
+import { useMemo } from "react"
 
 export type AudioRootState = {
     audioContext: AudioContext
-    isModuleLoaded: boolean
 }
 export type AudioRootStateAndMethod = AudioRootState & {
     dummy: () => void
@@ -11,21 +10,14 @@ export type AudioRootStateAndMethod = AudioRootState & {
 // import workerjs from "raw-loader!../../wasm/dist/index.js";
 
 export const useAudioRoot = (): AudioRootStateAndMethod => {
-    const [isModuleLoaded, setIsModuleLoaded] = useState<boolean>(false)
     const audioContext = useMemo(() => {
         const ctx = new AudioContext()
-        // const scriptUrl = URL.createObjectURL(new Blob([workerjs], { type: "text/javascript" }));
-        // ctx.audioWorklet.addModule(scriptUrl).then(() => {
-        //     console.log("voice-player-worklet-processor is loaeded.", ctx.audioWorklet)
-        setIsModuleLoaded(true)
-        // })
         return ctx
     }, [])
 
 
     return {
         audioContext,
-        isModuleLoaded,
         dummy: () => { }
     }
 }
